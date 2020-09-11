8b304ef267f349f396edfec06b50d7196744bde8
fabiohsm2008 <fabio.soto@ucsp.edu.pe>
2020-09-10 20:08:02 -0500

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,37 +1,88 @@
 #include <iostream>
+#include <fstream>
 #include "UDPLibrary.h"
 
 using namespace std;
 
-void error( char *msg)
+#define bufflen 1000
+
+class Client{
+private:
+    int sockfd;
+    sockaddr_in serv, client;
+    socklen_t l, m;
+public:
+    Client(int port, string ip){
+        sockfd = socket(AF_INET,SOCK_DGRAM,0);
+        serv.sin_family = AF_INET;
+        serv.sin_port = htons(port);
+        serv.sin_addr.s_addr = inet_addr(ip.c_str());
+        l = sizeof(client);
+        m = sizeof(serv);
+    }
+
+    void sendFile(string fileName){
+        ifstream file(fileName, ios::binary | ios::ate);
+        streamsize filesize = file.tellg();
+        file.seekg(0, ios::beg);
+
+        char buffer[bufflen];
+        bzero(buffer, bufflen);
+
+        while(file.read(buffer, bufflen) || file.gcount()){
+            sendto(sockfd, buffer, bufflen, 0, (struct sockaddr *)&serv, m);
+            bzero(buffer, bufflen);
+        }
+
+    }
+
+    void fec(char buffer1[bufflen], char buffer2[bufflen], char bufferResult[bufflen]){
+        for(int i = 0; i < bufflen; ++i){
+            bufferResult[i] == buffer1[i] ^ buffer2[i];
+        }
+    }
+};
+
+
+void error(char *msg)
 {
     perror(msg);
     exit(EXIT_FAILURE);
 }
-int main()
-{
-    int sockfd;
-    sockfd = socket(AF_INET,SOCK_DGRAM,0);
-    struct sockaddr_in serv,client;
 
-    serv.sin_family = AF_INET;
-    serv.sin_port = htons(8080);
-    serv.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    char buffer[256];
-    socklen_t l = sizeof(client);
+void sender(int sockfd, sockaddr_in serv){
     socklen_t m = sizeof(serv);
-    while (true)
-    {
-        cout<<"\ngoing to send\n";
-        cout<<"\npls enter the mssg to be sent\n";
-        fgets(buffer,256,stdin);
+    string msg;
+    cout << "Ready for send messagges" << endl;
+    while (true){
+        cin >> msg;
+        sendto(sockfd, msg.c_str(), msg.size(), 0, (struct sockaddr *)&serv, m);
+    }
+}
 
-        sendto(sockfd,buffer,sizeof(buffer),0,(struct sockaddr *)&serv,m);
-        bzero(buffer, 256);
-        recvfrom(sockfd,buffer,256,0,(struct sockaddr *)&client,&l);
-        cout << "Response from server: " << buffer << endl;
+void receiver(int sockfd, sockaddr_in client){
+    char buffer[bufflen];
+    bzero(buffer, bufflen);
+    socklen_t l = sizeof(client);
+    cout << "Ready for read messagges" << endl;
+    while (true){
+        recvfrom(sockfd, buffer, bufflen, 0, (struct sockaddr *)&client, &l);
+        cout << "Mensaje recibido: " << buffer << endl;
+        bzero(buffer, bufflen);
     }
     
+}
+
+void buildMsg(){
+
+}
+
+int main()
+{
+    Client r(8080, "127.0.0.1");
+    char buffer[10] = "hola";
+    char buffer2[10] = "como";
+    char buffer3[10];
+    r.fec(buffer, buffer2, buffer3);
     return 0;
 }
